9c6cd39d1b2a7589231d668f775231235cd2b385
Ruchi <ruchisaini.0404@gmail.com>
2022-02-03 15:30:16 +1100

    added routes'

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,150 +1,149 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Car, Booking } = require('../models');
-const { signToken } = require('../utils/auth');
-//const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+// const { AuthenticationError } = require('apollo-server-express');
+// const { User, Car, Booking } = require('../models');
+// const { signToken } = require('../utils/auth');
 
-const resolvers = {
-  Query: {
-    booking: async () => {
-      return await Booking.find();
-    },
-    bookings: async (parent, { car, name }) => {
-      const params = {};
+// const resolvers = {
+//   Query: {
+//     booking: async () => {
+//       return await Booking.find();
+//     },
+//     bookings: async (parent, { car, name }) => {
+//       const params = {};
 
-      if (car) {
-        params.car = car;
-      }
+//       if (car) {
+//         params.car = car;
+//       }
 
-      if (name) {
-        params.name = {
-          $regex: name
-        };
-      }
+//       if (name) {
+//         params.name = {
+//           $regex: name
+//         };
+//       }
 
-      return await Booking.find(params).populate('car');
-    },
-    car: async (parent, { _id }) => {
-      return await Car.findById(_id).populate('booking');
-    },
+//       return await Booking.find(params).populate('car');
+//     },
+//     car: async (parent, { _id }) => {
+//       return await Car.findById(_id).populate('booking');
+//     },
 
-    //done
-    user: async (parent, args, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'bookings.cars',
-          //populate: 'category'
-        });
+//     //done
+//     user: async (parent, args, context) => {
+//       if (context.user) {
+//         const user = await User.findById(context.user._id).populate({
+//           path: 'bookings.cars',
+//           //populate: 'category'
+//         });
 
-       // user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+//        // user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
 
-        return user;
-      }
+//         return user;
+//       }
 
-      throw new AuthenticationError('Not logged in');
-    },
+//       throw new AuthenticationError('Not logged in');
+//     },
     
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      const order = new Order({ products: args.products });
-      const line_items = [];
+//     checkout: async (parent, args, context) => {
+//       const url = new URL(context.headers.referer).origin;
+//       const order = new Order({ products: args.products });
+//       const line_items = [];
 
-      const { products } = await order.populate('products').execPopulate();
+//       const { products } = await order.populate('products').execPopulate();
 
-      for (let i = 0; i < products.length; i++) {
-        const product = await stripe.products.create({
-          name: products[i].name,
-          description: products[i].description,
-          images: [`${url}/images/${products[i].image}`]
-        });
+//       for (let i = 0; i < products.length; i++) {
+//         const product = await stripe.products.create({
+//           name: products[i].name,
+//           description: products[i].description,
+//           images: [`${url}/images/${products[i].image}`]
+//         });
 
-        const price = await stripe.prices.create({
-          product: product.id,
-          unit_amount: products[i].price * 100,
-          currency: 'usd',
-        });
+//         const price = await stripe.prices.create({
+//           product: product.id,
+//           unit_amount: products[i].price * 100,
+//           currency: 'usd',
+//         });
 
-        line_items.push({
-          price: price.id,
-          quantity: 1
-        });
-      }
+//         line_items.push({
+//           price: price.id,
+//           quantity: 1
+//         });
+//       }
 
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items,
-        mode: 'payment',
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`
-      });
+//       const session = await stripe.checkout.sessions.create({
+//         payment_method_types: ['card'],
+//         line_items,
+//         mode: 'payment',
+//         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+//         cancel_url: `${url}/`
+//       });
 
-      return { session: session.id };
-    }
-  },
-  Mutation: {
-      //corrected
-    addCar: async (parent, args) => {
-      const user = await Car.create(args);
-      const token = signToken(car);
+//       return { session: session.id };
+//     }
+//   },
+//   Mutation: {
+//       //corrected
+//     addCar: async (parent, args) => {
+//       const user = await Car.create(args);
+//       const token = signToken(car);
 
-      return { token, car };
-    },
+//       return { token, car };
+//     },
 
-    addOrder: async (parent, { products }, context) => {
-      console.log(context);
-      if (context.user) {
-        const order = new Order({ products });
+//     addOrder: async (parent, { products }, context) => {
+//       console.log(context);
+//       if (context.user) {
+//         const order = new Order({ products });
 
-        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+//         await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
 
-        return order;
-      }
+//         return order;
+//       }
 
-      throw new AuthenticationError('Not logged in');
-    },
-    //edit car
-    updateCar: async (parent, args, context) => {
-      if (context.car) {
-        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-      }
+//       throw new AuthenticationError('Not logged in');
+//     },
+//     //edit car
+//     updateCar: async (parent, args, context) => {
+//       if (context.car) {
+//         return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+//       }
 
-      throw new AuthenticationError('Not logged in');
-    },
-    updateProduct: async (parent, { _id, quantity }) => {
-      const decrement = Math.abs(quantity) * -1;
+//       throw new AuthenticationError('Not logged in');
+//     },
+//     updateProduct: async (parent, { _id, quantity }) => {
+//       const decrement = Math.abs(quantity) * -1;
 
-      return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
-    },
+//       return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
+//     },
 
-    //corrected
-    login: async (parent, { userName, password }) => {
-      const user = await User.findOne({ userName });
+//     //corrected
+//     login: async (parent, { userName, password }) => {
+//       const user = await User.findOne({ userName });
 
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
+//       if (!user) {
+//         throw new AuthenticationError('Incorrect credentials');
+//       }
 
-      const correctPw = await user.isCorrectPassword(password);
+//       const correctPw = await user.isCorrectPassword(password);
 
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
+//       if (!correctPw) {
+//         throw new AuthenticationError('Incorrect credentials');
+//       }
 
-      const token = signToken(user);
+//       const token = signToken(user);
 
-      return { token, user };
-    },
+//       return { token, user };
+//     },
 
-    //added
-    signup: async (root, { userName, password }) => {
-        try {
-          const user = await User.create({ userName, password });
-          const token = signToken(user);
-          return { token, user };
-        } catch (e) {
-          console.error(e);
-        }
-      },
-  }
-};
+//     //added
+//     signup: async (root, { userName, password }) => {
+//         try {
+//           const user = await User.create({ userName, password });
+//           const token = signToken(user);
+//           return { token, user };
+//         } catch (e) {
+//           console.error(e);
+//         }
+//       },
+//   }
+// };
 
-module.exports = resolvers;
+// module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,58 +1,58 @@
-const { gql } = require('apollo-server-express');
+// const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
+// const typeDefs = gql`
 
-  type Booking {
-    _id: ID
-    bookedTimeSlots: String
-    totalHours: Int
-    totalAmount: Int
-    transactionId: String
-    driverRequired: Boolean
-    user: User
-    car: Car
-  }
+//   type Booking {
+//     _id: ID
+//     bookedTimeSlots: String
+//     totalHours: Int
+//     totalAmount: Int
+//     transactionId: String
+//     driverRequired: Boolean
+//     user: User
+//     car: Car
+//   }
 
-  type Car {
-    _id: ID
-    name: String
-    image:String
-    capacity:Int
-    fuelType:String
-    bookedTimeSlots:String
-    rentPerHour:Int
-  }
+//   type Car {
+//     _id: ID
+//     name: String
+//     image:String
+//     capacity:Int
+//     fuelType:String
+//     bookedTimeSlots:String
+//     rentPerHour:Int
+//   }
 
-  type User {
-    _id: ID
-    userName: String
-  }
+//   type User {
+//     _id: ID
+//     userName: String
+//   }
 
-  type Checkout {
-    session: ID
-  }
+//   type Checkout {
+//     session: ID
+//   }
 
-  type Auth {
-    token: ID
-    user: User
-  }
+//   type Auth {
+//     token: ID
+//     user: User
+//   }
 
-  type Query {
-    booking: [Booking]
-    booking(car: ID, name: String): [Booking]
-    car(_id: ID!): Car
-    user: User
-    booking(_id: ID!): Booking
-  }
+//   type Query {
+//     booking: [Booking]
+//     booking(car: ID, name: String): [Booking]
+//     car(_id: ID!): Car
+//     user: User
+//     booking(_id: ID!): Booking
+//   }
 
-  type Mutation {
-    addUser(userName: String!, password: String!): Auth
-    addBooking(car: [ID]!): Booking
-    updateUser(userName: String, password: String): User
-    updateCar(_id: ID!, quantity: Int!): Car
-    login(userName: String!, password: String!): Auth
-    signup(userName: String!, password: String!): Auth
-  }
-`;
+//   type Mutation {
+//     addUser(userName: String!, password: String!): Auth
+//     addBooking(car: [ID]!): Booking
+//     updateUser(userName: String, password: String): User
+//     updateCar(_id: ID!, quantity: Int!): Car
+//     login(userName: String!, password: String!): Auth
+//     signup(userName: String!, password: String!): Auth
+//   }
+// `;
 
-module.exports = typeDefs;
+// module.exports = typeDefs;
