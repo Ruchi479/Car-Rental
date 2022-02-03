b87fc7dc0e3d819eb3a257db3eab4b41ce48f9a7
Ruchi <ruchisaini.0404@gmail.com>
2022-02-03 15:34:14 +1100

    updated server file

--- server/server.js
+++ server/server.js
@@ -1,21 +1,59 @@
-const express = require('express');
-const db = require('./config/connection');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const dbConnection = require('./config/connection');
+app.use(express.json())
+
+app.use('/api/cars/' , require('./routes/carsRoute'))
+app.use('/api/users/' , require('./routes/usersRoute'))
+app.use('/api/bookings/' , require('./routes/bookingsRoute'))
+
+
+const path = require('path')
+
+if(process.env.NODE_ENV==='production')
+{
+
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+
+    })
+
+}
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+ 
+
+
+app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
 
 
 
-//app.use(routes);
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// })
 
-app.get('/', (req, res) => res.send('Hello World'));
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
-    // log where we can go to test our GQL API
-   // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+// const express = require('express');
+// const db = require('./config/connection');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+
+
+// //app.use(routes);
+// // app.get('*', (req, res) => {
+// //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// // })
+
+// app.get('/', (req, res) => res.send('Hello World'));
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`🌍 Now listening on localhost:${PORT}`);
+//     // log where we can go to test our GQL API
+//    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
